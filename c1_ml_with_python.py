f79054cbb6a538a3ddf338043b6efe763e58cbad
hatemchaibi1111 <hatem.chaibi@mytoys.de>
2020-07-27 09:20:33 +0200

    update c1_ml_with_python.py

--- c1_ml_with_python.py
+++ c1_ml_with_python.py
@@ -1,23 +1,29 @@
 import pandas as pd
 import numpy as np
+import os
 import matplotlib.pyplot as plt
 import pylab as pl
+
+
 import urllib.request
 
-url = 'https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/ML0101ENv3/labs/FuelConsumptionCo2.csv'
 filename = 'FuelConsumption.csv'
-urllib.request.urlretrieve(url, filename)
+
+if not os.path.exists(filename):
+    url = 'https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/ML0101ENv3/labs/FuelConsumptionCo2.csv'
+    urllib.request.urlretrieve(url, filename)
 
 df = pd.read_csv(filename)
 
 df_described = df.describe()
 print(df_described)
 
-cdf = df[['ENGINESIZE','CYLINDERS','FUELCONSUMPTION_COMB','CO2EMISSIONS']]
+cdf = df[['ENGINESIZE', 'CYLINDERS', 'FUELCONSUMPTION_COMB', 'CO2EMISSIONS']]
 
+##
 #%% code cell
 
-viz = cdf[['CYLINDERS','ENGINESIZE','CO2EMISSIONS','FUELCONSUMPTION_COMB']]
+viz = cdf[['CYLINDERS', 'ENGINESIZE', 'CO2EMISSIONS', 'FUELCONSUMPTION_COMB']]
 viz.hist()
 plt.show()
 
