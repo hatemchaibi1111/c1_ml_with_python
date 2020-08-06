6010cbd04051bc24a94c2580c9d964146e22eab3
hatemchaibi1111 <hatem.chaibi@mytoys.de>
2020-08-06 07:44:24 +0200

    update c1_ml_with_python.py

--- c1_ml_with_python.py
+++ c1_ml_with_python.py
@@ -22,9 +22,33 @@ cdf = df[['ENGINESIZE', 'CYLINDERS', 'FUELCONSUMPTION_COMB', 'CO2EMISSIONS']]
 
 ##
 #%% code cell
-
 viz = cdf[['CYLINDERS', 'ENGINESIZE', 'CO2EMISSIONS', 'FUELCONSUMPTION_COMB']]
 viz.hist()
 plt.show()
 
-print(df.head())
+##
+plt.scatter(cdf.FUELCONSUMPTION_COMB, cdf.CO2EMISSIONS,  color='blue')
+plt.xlabel("FUELCONSUMPTION_COMB")
+plt.ylabel("Emission")
+plt.show()
+
+##
+plt.scatter(cdf.ENGINESIZE, cdf.CO2EMISSIONS,  color='blue')
+plt.xlabel("Engine size")
+plt.ylabel("Emission")
+plt.show()
+
+##
+plt.scatter(cdf.CYLINDERS, cdf.CO2EMISSIONS,  color='red')
+plt.xlabel("CYLINDERS")
+plt.ylabel("Emission")
+plt.show()
+
+##
+# Creating train and test dataset
+
+msk = np.random.rand(len(df)) < 0.8
+train = cdf[msk]
+test = cdf[~msk]
+
+print()
