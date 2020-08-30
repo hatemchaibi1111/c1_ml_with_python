8f0dc2984699b428b7edb5082c9c2ef857e5634a
hatemchaibi1111 <hatem.chaibi@mytoys.de>
2020-08-30 16:19:55 +0200

    add c1w2_03_ml_non_lin_regression.py

--- c1w2_03_ml_non_lin_regression.py
+++ c1w2_03_ml_non_lin_regression.py
@@ -13,13 +13,82 @@ if not os.path.exists(filename):
     urllib.request.urlretrieve(url, filename)
 
 df = pd.read_csv("china_gdp.csv")
-print(df.head(10))
+print('\nchina_gdp.csv:\n' + str(df.head(10)) + '\n...\n')
 
-plt.figure(figsize=(8,5))
+
+fig, axs = plt.subplots(2, 2)
+
 x_data, y_data = (df["Year"].values, df["Value"].values)
-plt.plot(x_data, y_data, 'ro')
-plt.ylabel('GDP')
-plt.xlabel('Year')
-plt.show()
+axs[0][0].plot(x_data, y_data, 'ro')
+axs[0][0].set_ylabel('GDP')
+axs[0][0].set_xlabel('Year')
+plt.show(block=False)
 
+X = np.arange(-5.0, 5.0, 0.1)
+Y = 1.0 / (1.0 + np.exp(-X))
+
+axs[0][1].plot(X, Y)
+axs[0][1].set_ylabel('Dependent Variable')
+axs[0][1].set_xlabel('Indepdendent Variable')
+plt.show(block=False)
+
+
+def sigmoid(x, Beta_1, Beta_2):
+    y = 1 / (1 + np.exp(-Beta_1 * (x - Beta_2)))
+    return y
+
+
+beta_1 = 0.10
+beta_2 = 1990.0
+
+# logistic function
+Y_pred = sigmoid(x_data, beta_1, beta_2)
+
+# plot initial prediction against datapoints
+axs[1][0].plot(x_data, Y_pred*15000000000000.)
+axs[1][0].plot(x_data, y_data, 'ro')
+
+plt.show(block=False)
+
+# normalization
+xdata =x_data/max(x_data)
+ydata =y_data/max(y_data)
+
+from scipy.optimize import curve_fit
+popt, pcov = curve_fit(sigmoid, xdata, ydata)
+
+# print the final parameters
+print("beta_1 = %f, \nbeta_2 = %f" % (popt[0], popt[1]))
+
+x = np.linspace(1960, 2015, 55)
+x = x/max(x)
+
+y = sigmoid(x, *popt)
+axs[1][1].plot(xdata, ydata, 'ro', label='data')
+axs[1][1].plot(x,y, linewidth=3.0, label='fit')
+axs[1][1].legend(loc='best')
+axs[1][1].set_ylabel('GDP')
+axs[1][1].set_xlabel('Year')
+plt.show(block=False)
+
+# split data into train/test
+msk = np.random.rand(len(df)) < 0.8
+train_x = xdata[msk]
+test_x = xdata[~msk]
+train_y = ydata[msk]
+test_y = ydata[~msk]
+
+# build the model using train set
+popt, pcov = curve_fit(sigmoid, train_x, train_y)
+
+# predict using test set
+y_hat = sigmoid(test_x, *popt)
+
+# evaluation
+print("Mean absolute error: %.2f" % np.mean(np.absolute(y_hat - test_y)))
+print("Residual sum of squares (MSE): %.2f" % np.mean((y_hat - test_y) ** 2))
+from sklearn.metrics import r2_score
+print("R2-score: {:.2f}".format(r2_score(y_hat , test_y)))
+
+plt.show()
 print()
---
+++ c1w3_01_k_nearest_neighbors.py
@@ -0,0 +1,85 @@
+import itertools
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.ticker import NullFormatter
+import pandas as pd
+import numpy as np
+import matplotlib.ticker as ticker
+from sklearn import preprocessing
+import os
+import urllib.request
+
+pd.set_option('display.width', 0)
+
+filename = 'teleCust1000t.csv'
+if not os.path.exists(filename):
+    url = ('https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/ML0101ENv3/labs/'
+           'teleCust1000t.csv')
+    urllib.request.urlretrieve(url, filename)
+
+df = pd.read_csv("teleCust1000t.csv")
+
+print('\nteleCust1000t.csv\n' + str(df.head()) + '\n...\n')
+
+df.hist(column='income', bins=50)
+
+plt.show(block=False)
+
+##
+
+X = df[['region', 'tenure', 'age', 'marital', 'address', 'income', 'ed', 'employ', 'retire', 'gender', 'reside']].values
+
+# Normalize Data
+X = preprocessing.StandardScaler().fit(X).transform(X.astype(float))
+y = df['custcat'].values
+
+##
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=4)
+print('Train set:', X_train.shape,  y_train.shape)
+print('Test set:', X_test.shape,  y_test.shape)
+
+from sklearn.neighbors import KNeighborsClassifier
+k = 4
+# Train Model and             Predict
+neigh = KNeighborsClassifier(n_neighbors = k).fit(X_train,y_train)
+yhat = neigh.predict(X_test)
+
+
+from sklearn import metrics
+print("Train set Accuracy: ", metrics.accuracy_score(y_train, neigh.predict(X_train)))
+print("Test set Accuracy: ", metrics.accuracy_score(y_test, yhat))
+
+neigh2 = KNeighborsClassifier(n_neighbors=6).fit(X_train,y_train)
+yhat2 = neigh.predict(X_test)
+print("Train set Accuracy2: ", metrics.accuracy_score(y_train, neigh.predict(X_train)))
+print("Test set Accuracy2: ", metrics.accuracy_score(y_test, yhat))
+
+
+# accuracy of KNN for different Ks
+Ks = 10
+mean_acc = np.zeros((Ks - 1))
+std_acc = np.zeros((Ks - 1))
+ConfustionMx = [];
+for n in range(1, Ks):
+    # Train Model and Predict
+    neigh = KNeighborsClassifier(n_neighbors=n).fit(X_train, y_train)
+    yhat = neigh.predict(X_test)
+    mean_acc[n - 1] = metrics.accuracy_score(y_test, yhat)
+
+    std_acc[n - 1] = np.std(yhat == y_test) / np.sqrt(yhat.shape[0])
+
+
+plt.figure(2)
+plt.plot(range(1, Ks), mean_acc, 'g')
+plt.fill_between(range(1, Ks), mean_acc - 1 * std_acc, mean_acc + 1 * std_acc, alpha=0.10)
+plt.legend(('Accuracy ', '+/- 3xstd'))
+plt.ylabel('Accuracy ')
+plt.xlabel('Number of Nabors (K)')
+plt.tight_layout()
+plt.show(block=False)
+
+print("The best accuracy was with", mean_acc.max(), "with k =", mean_acc.argmax()+1)
+
+plt.show()
+print()
